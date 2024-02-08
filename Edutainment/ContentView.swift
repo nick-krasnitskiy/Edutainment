febb22432cd989bb68b50f2a69b3e022c31c90c2
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-02-08 10:21:38 +0300

    Update UI

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isGameActive = false
+    @State private var multipleTable: Int = 2
+    @State private var questionAmount: Int = 5
+    @State private var answer: Int? = nil
+    
+    let numberOfQuestions = [5, 10, 20]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        if isGameActive {
+            NavigationStack {
+                VStack(spacing: 15) {
+                    Text("What is \(multipleTable) x \(Int.random(in: 1...multipleTable)) ?")
+                        .font(.title)
+                    TextField("Enter your answer",  value: $answer, format: .number)
+                        .multilineTextAlignment(.center)
+                }
+                .toolbar {
+                    Button("Change settings") { isGameActive = false }
+                }
+            }
+        } else {
+            NavigationStack {
+                List {
+                    Section ("Choose the type of multiplication table") {
+                        
+                        Stepper("\(multipleTable) multiplication table", value: $multipleTable, in: 2...12)
+                    }
+                    
+                    Section ("Chose the number of questions") {
+                        Picker("", selection: $questionAmount) {
+                            ForEach(numberOfQuestions, id: \.self) {
+                                Text("\($0)")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    
+                    Section ("Save setting") {
+                        Button ("Save and Play") { isGameActive = true }
+                    }
+                }
+                .listStyle(.grouped)
+                .navigationTitle("Game settings")
+            }
         }
-        .padding()
     }
 }
 
